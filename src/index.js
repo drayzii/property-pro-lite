a32c0c1dafc127ee3d530d26a928d001af47a24a
Jonathan Shyaka <jonashyaka2@gmail.com>
2019-10-25 09:44:47 +0200

    [chore]: set up react with webpack

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { render } from "react-dom";
+import './index.scss';
+
+render(
+  <div className="cool">Hello World</div>,
+  document.getElementById("root")
+);
---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require('path');
+
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: "./src/index.html",
+  filename: "./index.html"
+});
+
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve('build'),
+    filename: 'index.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ]
+  },
+  plugins: [htmlPlugin]
+};
